9a00b907d3863cc2fea3d91ccf255f4d1fc86a4a
hugomos <vitor_osantos@hotmail.com>
2023-03-24 14:52:36 -0300

    styles: melhorando espaçamento da tela home. @hugomos

--- src/core/pages/Home/styles.ts
+++ src/core/pages/Home/styles.ts
@@ -15,8 +15,16 @@ export const Content = styled.main`
   width: 100%;
   padding: 1.6rem;
 
-  @media screen and (min-width: 768px) {
-    padding: 3.2rem 4.2rem;
+  @media screen and (min-width: 650px) {
+    padding: 3.2rem 6.2rem;
+  }
+
+  @media screen and (min-width: 780px) {
+    padding: 3.2rem 10.2rem;
+  }
+
+  @media screen and (min-width: 995px) {
+    padding: 3.2rem 16.2rem;
   }
 `
 
