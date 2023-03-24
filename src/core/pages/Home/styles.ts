2d1c9c0eaafc8dfd929ad2a88719c5e416a8cf52
hugomos <vitor_osantos@hotmail.com>
2023-03-24 15:03:05 -0300

    styles: removendo espaçamento excessivo. @hugomos

--- src/core/pages/Home/styles.ts
+++ src/core/pages/Home/styles.ts
@@ -13,18 +13,11 @@ export const Container = styled.div<ContainerProps>`
 
 export const Content = styled.main`
   width: 100%;
-  padding: 1.6rem;
+  /* padding: 1.6rem; */
+  padding: 2.4rem 2.6rem;
 
-  @media screen and (min-width: 650px) {
-    padding: 3.2rem 6.2rem;
-  }
-
-  @media screen and (min-width: 780px) {
-    padding: 3.2rem 10.2rem;
-  }
-
-  @media screen and (min-width: 995px) {
-    padding: 3.2rem 16.2rem;
+  @media screen and (min-width: 768px) {
+    padding: 3.2rem 4.2rem;
   }
 `
 
